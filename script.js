6a7defa9fb87d6df302e78055cea6b924888613f
Mariana Hodniki Davie <marianahodniki1991@gmail.com>
2022-06-05 14:25:30 -0600

    added darkMode function

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-var checkbox = document.getElementById("Changetheme");
+var checkbox = document.getElementById("Changetheme");// get checkbox to variable
+
+//check storage if dark mode was on or off and
+
+if (sessionStorage.getItem("mode")== "dark"){
+    darkMode(); // if dark mode is on, run this function
+
+} else {
+    nodark(); // else run this function
+}
+
+// if the checkbox state is changed, ruh this other function
+
+if (checkbox.checked) {
+    darkmode(); // if the checkbox is checked, run this function
+
+} else {
+    nodark(); // else run this function
+}
+
+// function for checkbox when checkbox is checked
+
+function darkMode() {
+    document.body.classList.add("dark-mode");
+    // add a class to the body tag
+    checkbox.checked = true; //set checkbox to be checked state
+    
+}
