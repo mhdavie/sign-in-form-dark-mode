89c444e5b5dfc2f0fc375c9695963e785fd131e3
Mariana Hodniki Davie <marianahodniki1991@gmail.com>
2022-06-06 21:25:21 -0600

    consolo log

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var checkbox = document.getElementById("Changetheme");// get checkbox to variabl
 
 //check storage if dark mode was on or off and
 
-if (sessionStorage.getItem("mode")== "dark"){
+if (sessionStorage.getItem("mode") == "dark"){
     darkMode(); // if dark mode is on, run this function
 
 } else {
@@ -12,7 +12,8 @@ if (sessionStorage.getItem("mode")== "dark"){
 // if the checkbox state is changed, ruh this other function
 
 if (checkbox.checked) {
-    darkmode(); // if the checkbox is checked, run this function
+    darkMode();
+    console.log("hey") // if the checkbox is checked, run this function
 
 } else {
     nodark(); // else run this function
@@ -31,8 +32,8 @@ function darkMode() {
 
 function nodark() {
 
-    document.bodyclassList.remove("dark-mode");//remove  added class from body tag
+    document.bodyclassList.remove("dark-mode"); //remove  added class from body tag
     checkbox.checked = false; // set checked box to unchecked state
     sessionStorage.setItem("mode", "light"); //store a name & value to know that dark mode is off or light mode is on
-    
+
 }
