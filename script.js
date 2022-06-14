8d6ef7fc74f67cef04b7868f18abbcb2ae9046fb
Mariana Hodniki Davie <marianahodniki1991@gmail.com>
2022-06-14 16:12:53 -0600

    final update

--- script.js
+++ script.js
@@ -2,14 +2,19 @@
 const  inputContainer = document.querySelector('input')
 // console.log(inputContainer)
 
+const rootElement = document.documentElement
+// console.log(rootElement)
+
+
+
 const lightTheme = {
     'background': '#1B2029',
     'color': 'white'
 }
 
 const darkTheme = {
-    'background-color': 'white',
-    'color': 'black'
+    '--background-color': 'white',
+    '--text-color': 'black'
 
 }
 
@@ -22,7 +27,21 @@ inputContainer.addEventListener('change', function() {
 })
 
 function changeTheme(theme) {
+    // alterar o valor da propriedade
+    rootElement.style.setProperty('--background-color', 'white')
     
+    for (let prop in theme) {
+        console.log(prop)
+
+    }
+
+
+
+    
+}
+
+function changeProperty(property, value) {
+    rootElement.style.setProperty(property, value) 
 }
 
   
