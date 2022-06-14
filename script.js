c741e5d9437368006f30efe1fd20d87f29c62fa2
Mariana Hodniki Davie <marianahodniki1991@gmail.com>
2022-06-13 20:40:43 -0600

    new logic added

--- script.js
+++ script.js
@@ -1,59 +1,27 @@
 
 const  inputContainer = document.querySelector('input')
+// console.log(inputContainer)
 
 const lightTheme = {
     'background': '#1B2029',
     'color': 'white'
-
 }
 
-const DarkTheme = {
+const darkTheme = {
     'background-color': 'white',
     'color': 'black'
 
 }
 
 inputContainer.addEventListener('change', function() {
-  const ischecked = inputContainer.checked
+    const ischecked = inputContainer.checked
 
+    ischecked ? changeTheme(darkTheme) : changeTheme(lightTheme)
+    console.log(changeTheme(darkTheme))
+    
 })
 
+  
 
-// var checkbox = document.getElementById("Changetheme");// get checkbox to variable
 
-// //check storage if dark mode was on or off and
 
-// if (sessionStorage.getItem("mode") == "dark"){
-//     darkMode(); // if dark mode is on, run this function
-
-// } else {
-//     nodark(); // else run this function
-// }
-
-// // if the checkbox state is changed, ruh this other function
-
-// if (checkbox.checked) {   
-//     console.log("hey") // if the checkbox is checked, run this function
-
-// } else {
-//     nodark(); // else run this function
-// }
-
-// // function for checkbox when checkbox is checked
-
-// function darkMode() {
-//     document.body.classList.add("dark-mode");
-//     // add a class to the body tag
-//     checkbox.checked = true; //set checkbox to be checked state
-//     sessionStorage.setItem("mode", "dark"); //store a name & value to know that dark mode is on
-// }
-
-// // function for checkbox when checkbox is not checked
-
-// function nodark() {
-
-//     document.body.classList.remove("dark-mode"); //remove  added class from body tag
-//     checkbox.checked = false; // set checked box to unchecked state
-//     sessionStorage.setItem("mode", "light"); //store a name & value to know that dark mode is off or light mode is on
-
-// }
